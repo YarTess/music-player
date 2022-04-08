7739c9dad4f5a5e524f3ec3e914c7ed3c1d36e62
Ademok <78264481+Ademok@users.noreply.github.com>
2022-04-08 10:45:36 +0600

    Add files via upload

---
+++ models.py
@@ -0,0 +1,58 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+class Song(models.Model):
+
+    Genre_Choice = (
+            ('Pop', 'Pop'),
+            ('Rock', 'Rock'),
+
+          )
+
+    name = models.CharField(max_length=200)
+    album = models.CharField(max_length=200)
+    genre = models.CharField(max_length=20, choices=Genre_Choice, default='Pop')
+    song_img = models.FileField()
+    year = models.IntegerField()
+    singer = models.CharField(max_length=200)
+    song_file = models.FileField()
+
+    def __str__(self):
+        return self.name
+
+
+class Playlist(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    playlist_name = models.CharField(max_length=200)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+
+class Favourite(models.Model):
+    id = models.AutoField(primary_key=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    is_fav = models.BooleanField(default=False)
+
+
+class Recent(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+
+
+class Comments(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    song = models.ForeignKey(Song, on_delete=models.CASCADE)
+    comment = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+
+
+class Album(models.Model):
+    artist = models.CharField(max_length=100)
+    album_title = models.CharField(max_length=50)
+    genre = models.CharField(max_length=50)
+    released_year = models.IntegerField()
+
+    def __str__(self):
+        return self.album_title + '-' + self.artist
